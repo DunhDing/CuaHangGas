09e5ef28cca13ca9aeb6c23dc5df7e07c7feb990
DunhDing <tuandung15102005@gmail.com>
2025-06-08 09:28:00 +0700

    update tongquan

--- src/main/java/com/tuandat/cuahanggas/ui/ucTongQuan.java
+++ src/main/java/com/tuandat/cuahanggas/ui/ucTongQuan.java
@@ -1,5 +1,6 @@
 package com.tuandat.cuahanggas.ui;
 
+import java.awt.Color;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
@@ -9,10 +10,12 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import org.jfree.chart.plot.CategoryPlot;
+import org.jfree.chart.renderer.category.BarRenderer;
 
 public class ucTongQuan extends javax.swing.JPanel {
 
-    private Connection conn;  
+    private Connection conn;
 
     public ucTongQuan(Connection conn) {
         this.conn = conn;
@@ -55,14 +58,17 @@ public class ucTongQuan extends javax.swing.JPanel {
             // Tạo biểu đồ từ dữ liệu cơ sở dữ liệu
             JFreeChart chart = ChartFactory.createBarChart(
                     "Doanh số bình gas hôm nay", // Tiêu đề biểu đồ
-                    "Tên Bình Gas (Loại)", // Tiêu đề trục X
-                    "Số Lượng Xuất", // Tiêu đề trục Y
+                    "Tên bình gas (Loại)", // Tiêu đề trục X
+                    "Số lượng xuất", // Tiêu đề trục Y
                     dataset // Dữ liệu biểu đồ
             );
-
+            CategoryPlot plot = chart.getCategoryPlot();
+            BarRenderer renderer = (BarRenderer) plot.getRenderer();
+            renderer.setMaximumBarWidth(0.1); // Đặt độ rộng tối đa của cột thành 10%
+            renderer.setSeriesPaint(0, new Color(0,102,204));
             // Thêm biểu đồ vào panel
             ChartPanel chartPanel = new ChartPanel(chart);
-            chartPanel.setPreferredSize(new java.awt.Dimension(800, 600));
+            chartPanel.setPreferredSize(new java.awt.Dimension(800, 400));
             this.setLayout(new java.awt.BorderLayout());
             this.add(chartPanel, java.awt.BorderLayout.CENTER);
 
